9786cf26ce8bdcb6b484423cf40c497789d392a9
lucasbalbino <lucabmf@outlook.com>
2016-09-07 23:16:11 -0300

    Commit inicial

---
+++ public/js/controllers/ContatosController.js
@@ -0,0 +1,9 @@
+angular.module('contatooh').controller('ContatosController',
+	function ($scope) {
+		$scope.total = 0;
+
+		$scope.incrementa = function () {
+			$scope.total++;
+		}
+	}
+);
---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+angular.module('contatooh', ['ngRoute'])
+	.config(function ($routeProvider) {
+		
+		$routeProvider.when('/contatos', {
+			templateUrl: 'partials/contatos.html',
+			controller: 'ContatosController'
+		})
+		.when('/contato/:contatoId', {
+			templateUrl: 'partials/contato.html',
+			controller: 'ContatoController'
+		})
+		.otherwise({redirectTo: '/contatos'});
+	});
